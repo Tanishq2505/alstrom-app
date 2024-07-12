716adea488926c844e37713e6ce9ced62ec75550
Tanishq Kashyap <tankashy@dhcp-10-120-156-77.wifi.oracle.com>
2024-07-12 09:41:12 +0530

    initial commit

---
+++ src/main/java/com/training/bms/AlstronAppApplication.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstronAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstronAppApplication.class, args);
+		System.out.println("Alstrom app started");
+	}
+
+}
